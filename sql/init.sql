13578a0cdd924e3e99ab7363bca1957e42d889cb
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2020-03-02 23:55:56 +0700

    docker

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,13 @@
-create table `like`.`Like`
+create database `like`;
+
+create table `Like`
 (
 	id int auto_increment
 		primary key,
 	review_id int not null,
 	student_id varchar(256) not null,
-	date datetime null
+	date datetime null,
+	constraint Like_review_id_student_id_uindex
+		unique (review_id, student_id)
 );
 
